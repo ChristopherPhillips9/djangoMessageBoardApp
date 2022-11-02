30421ae7ee51896b4dc5cfd906efdae47f5e6dec
ChristopherPhillips9 <112596258+ChristopherPhillips9@users.noreply.github.com>
2022-11-01 22:36:20 -0400

    Created home.html. Progress up to page 85

--- messageBoard/views.py
+++ messageBoard/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView
+
+from .models import MessageBoard
+
+class HomePageView(ListView):
+    model = MessageBoard
+    template_name = "home.html"
+
+
