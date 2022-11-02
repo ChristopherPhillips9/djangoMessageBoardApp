96742eeb118ac818cb082f92be7f5c0025790b32
ChristopherPhillips9 <112596258+ChristopherPhillips9@users.noreply.github.com>
2022-11-01 21:50:54 -0400

    If statement added to the messageboard model

--- messageBoard/models.py
+++ messageBoard/models.py
@@ -6,3 +6,11 @@ from django.db import models
 
 class MessageBoard(models.Model):
     text = models.TextField()
+
+    def __str__(self):
+        # Just trying something fun. if the text entry is "Hello, World!" then it changes the title in the admin console
+        # if you're reading this Arana Fireheart, then hello!
+        if self.text[:13] == "Hello, World!":
+            return "Typical Hello World Message smh..."
+        else:
+            return self.text[:50]
